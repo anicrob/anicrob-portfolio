3f102b86ca488b986bbca4a6ec55d2b99ef82b9e
goose <anrobbins18@gmail.com>
2023-08-08 18:42:01 -0400

    added header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import Navigation from "./Navigation";
+import headerStyles from "../css/header.module.css";
 
 export default function Header() {
   return <header>
-    <h1 className='ms-1'>Andrea</h1>
+    <h1 className={`${headerStyles.title}`}>Andrea</h1>
     <Navigation />
   </header>
 }
